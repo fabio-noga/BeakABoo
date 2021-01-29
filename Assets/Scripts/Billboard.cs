00ffcc554f997860af7ff1bfecd9aa13a2197cd1
barbosayyy <pedro_barbosa2000@hotmail.com>
2021-01-29 19:49:34 +0000

    0.1

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Camera cam;
+
+    public bool useStaticBillboard;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+    
+    void LateUpdate()
+    {
+        if (!useStaticBillboard)
+        {
+            transform.LookAt(cam.transform);
+        }
+
+        else
+        {
+            transform.rotation = cam.transform.rotation;
+        }
+
+        transform.rotation = Quaternion.Euler(-20, transform.rotation.eulerAngles.y, 0f);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    void Update()
+    {
+        float time = Time.deltaTime;
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * time);
+    }
+}
