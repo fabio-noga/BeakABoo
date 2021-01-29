cfa645a4eb71ff42521736105653609ad6982486
Noga <fabiomiguel98@hotmail.com>
2021-01-29 22:50:27 +0000

    Camera smoothness & Follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    
+    public Transform target;
+    public float smoothSpeed = 2.5f;
+    public Vector3 DistanceToPlayer;
+    private Vector2 movement;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + DistanceToPlayer;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+    }
+    private void FixedUpdate()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement = movement.normalized;
+        //UP
+        /*if (movement.x == 0 && movement.y > 0)
+            desiredPosition = target.position + DistanceToPlayer + new Vector3(5f * Time.deltaTime, 0, 5f * Time.deltaTime);
+        //RIGHT
+        else if (movement.x > 0 && movement.y == 0)
+            desiredPosition = target.position + DistanceToPlayer + new Vector3(-5f * Time.deltaTime, 0, -5f * Time.deltaTime);
+        //DOWN
+        else if (movement.x == 0 && movement.y < 0)
+            firePoint.transform.position = transform.position + new Vector3(0f, -0.8f);
+        //LEFT
+        else if (movement.x < 0 && movement.y == 0)
+            firePoint.transform.position = transform.position + new Vector3(-0.7f, -0.3f);
+        */
+    }
+
 }
---
+++ Assets/Scripts/NPCFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCFollow : MonoBehaviour
+{
+
+
+    public Transform target;
+    NavMeshAgent nav;
+    private NavMeshAgent navMeshAgent;
+    public float closeEnoughMeters = 0.4f;
+    public float acceleration = 2f;
+    public float deceleration = 60f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        nav.SetDestination(target.position);
+        navMeshAgent = gameObject.GetComponentInChildren<NavMeshAgent>();
+        if (navMeshAgent.hasPath)
+            navMeshAgent.acceleration = (navMeshAgent.remainingDistance < closeEnoughMeters) ? deceleration : acceleration;
+
+    }
+}
