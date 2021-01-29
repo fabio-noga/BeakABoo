46e34d053585cb08c6389c94f9a58bcc0f128b25
barbosayyy <pedro_barbosa2000@hotmail.com>
2021-01-29 23:09:15 +0000

    Gravity & Fbx

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
     public float speed = 6f;
+    public float gravity = -9.81f;
+
+    private Vector3 velocity;
 
     void Update()
     {
@@ -17,5 +21,9 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * time);
+
+        velocity.y += gravity * time;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
