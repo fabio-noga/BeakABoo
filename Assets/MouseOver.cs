20060817bd49d213175a3013424db668a68205dd
Noga <fabiomiguel98@hotmail.com>
2021-01-31 04:02:58 +0000

    Title Screen

---
+++ Assets/MouseOver.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class MouseOver : MonoBehaviour
+{
+    SpriteRenderer button;
+    Sprite sprite;
+    Sprite sprite2;
+
+    void Start(){
+        button = gameObject.GetComponent<SpriteRenderer>();
+        sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+        sprite2 = Resources.Load<Sprite>("selectedoption");
+    }
+    void OnMouseOver(){
+        button.sprite = sprite2;
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    void OnMouseExit() {
+        button.sprite = sprite;
+    }
+}
---
+++ Assets/SlowdownBush.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowdownBush : MonoBehaviour
+{
+    Animator m_Animator;
+    public float speed = 0.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Animator.speed = speed;
+    }
+}
